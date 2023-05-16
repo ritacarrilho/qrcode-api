e0252b685ee26b6e9783e40d37d74b76ea17d4ce
Rita Carrilho <76398242+ritacarrilho@users.noreply.github.com>
2023-05-16 17:38:31 +0200

    Create controller_test.py
    tested encode methods

---
+++ controller_test.py
@@ -0,0 +1,22 @@
+# Import pyqrcode
+import pyqrcode
+
+class QRCodeBuilder:
+  # Generate QR code
+  def generate_qr(url: str):
+    qr_code = pyqrcode.create(url)
+    
+    # img = qr_code.png("test.png", scale=8)
+    qrcode_encoded = encode(qr_code)
+    # print in the terminal
+    print(qr_code.terminal())
+    
+    image_as_str = qr_code.png_as_base64_str(scale=5)
+    # print code encoded base64
+    print(image_as_str)
+    
+    # binary
+    text = qr_code.text()
+    print(text)
+    
+    return qrcode_encoded
