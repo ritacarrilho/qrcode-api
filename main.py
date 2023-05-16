35a90420ea928ef7680a60a48306997fb87fb889
Rita Carrilho <76398242+ritacarrilho@users.noreply.github.com>
2023-05-16 17:42:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 from qr_controller import generate_qr
+import QRCodeBuilder 
 
 app = FastAPI()
 
 @app.get("/{url}")
 async def generate_qr_code(url: str):
+    QRCodeBuilder.generate_qr(url)
     qr_code = generate_qr(url)
     # print(type(qr_code))
     return qr_code
